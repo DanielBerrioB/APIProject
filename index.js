382f62cead67b7238181b4eb68a9f2e910be8272
Your Nam <you@example.com>
2019-04-05 21:11:13 -0500

    MongoDB added

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var bodyParser = require("body-parser");
 var app = express();
 
 app.use(bodyParser.json());
-app.use('/', require('./routes/users'));
+app.use("/", require("./routes/users"));
 
 //Get the server port or a default port called 3000
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-    console.log(`I'm listening port ${port}`);
+  console.log(`I'm listening port ${port}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,45 @@
 var express = require("express");
+var MongoClient = require("mongodb").MongoClient;
+var cors = require("cors");
 var app = express();
+app.use(cors());
 
-var arrayAux = []; //This will be deleted when the DB is installed
+var collection1 = "users";
+var nameDB = "boutique";
+var url =
+  "mongodb+srv://DanielBB:DanielBB@cluster0-qdjmv.mongodb.net/test?retryWrites=true";
 
 //This method is used to get alla the data
-app.get("/users/", (req, res) => {
-    res.status(400).send(arrayAux);
+app.get("/users/create/", (req, res) => {
+  MongoClient.connect(url, (err, dataBase) => {
+    if (err) throw err;
+    const db = dataBase.db(nameDB);
+    db.collection(collection1)
+      .find()
+      .toArray((err, result) => {
+        if (err) throw err;
+        res.status(200).send(result);
+        //db.close(); That's not working
+      });
+  });
 });
 
 //This POST method allows to add some users to the API
-app.post("/users/", (req, res) => {
-    var body = req.body;
-    arrayAux.push(body);
-    res.status(201).send(arrayAux);
+app.post("/users/create/", (req, res) => {
+  var body = req.body;
+
+  MongoClient.connect(url, (err, dataBase) => {
+    if (err) throw err;
+    const db = dataBase.db(nameDB); //The connection with the database is establish
+    db.collection(collection1).insert(body);
+    db.collection(collection1)
+      .find()
+      .toArray((err, result) => {
+        if (err) throw err;
+        res.status(201).send(result);
+        //db.close(); That's not working
+      });
+  });
 });
 
 module.exports = app; //To export the app
