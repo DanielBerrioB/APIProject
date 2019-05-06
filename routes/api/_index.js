ebc0f4a555264b631085fb1007d4081c3e741f73
Your Nam <you@example.com>
2019-05-06 18:21:48 -0500

    API finished

--- routes/api/_index.js
+++ routes/api/_index.js
@@ -8,4 +8,10 @@ app.use("/", require("./clothes"));
 app.use("/", require("./users"));
 app.use("/", require("./shopCart"));
 
+//If the route is not found, this message is deploy
+app.use((req, res, next) => {
+  res.status(400).send({
+    message: "This end point does not exits"
+  });
+});
 module.exports = app;
